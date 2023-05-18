5c1742017fd21ed0ce68211ab2016dd34ce80fcf
Amanda Viória <amandavic7576@gmail.com>
2023-05-18 15:33:48 -0300

    Começando map

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,61 @@
-import Pagina from '@/components/Pagina'
-import React from 'react'
-import styles from '@/styles/Home.module.css'
-import { Button, Col, Row } from 'react-bootstrap'
+import Pagina from '@/components/Pagina';
+import React from 'react';
+import styles from '@/styles/Home.module.css';
+import { Button, Col, Row } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from '@/components/Card';
 import Head from 'next/head';
+import apiDeputados from '@/services/apiDeputados';
 
-
-const index = () => {
+const Index = ({ deputados }) => {
   return (
     <>
-    <Head>
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-    <link href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
-    </Head>
-    <Pagina>
-      <br/>
-      <Row>
-          <Col md={6} className='text-white '>
-              <h2>De Olho nos Gastos:<br></br>Fiscalizando as<br></br>Despesas dos<br></br>Deputados Brasileiros</h2>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Pagina>
+        <br />
+        <Row>
+          <Col md={6} className="text-white ">
+            <h2>
+              De Olho nos Gastos:
+              <br />
+              Fiscalizando as
+              <br />
+              Despesas dos
+              <br />
+              Deputados Brasileiros
+            </h2>
           </Col>
           <Col></Col>
-      </Row>
-      <br></br>
-      <Button className='bg-secondary text-white
-      ' variant='outline-dark'>Explore</Button>
+        </Row>
+        <br />
+        <Button className="bg-secondary text-white" variant="outline-dark">
+          Explore
+        </Button>
 
-      <section className={styles.deputados}>
-            <Card />
-      </section>
-    </Pagina>
+        <section className={styles.deputados}>
+          {deputados.map((item) => (
+            <Card key={item.id} nome={item.nome} estado={item.siglaUF} />
+          ))}
+        </section>
+      </Pagina>
     </>
-  )
-}
+  );
+};
 
-export default index
+export default Index;
+
+export async function getServerSideProps(context) {
+  const response = await apiDeputados.get('/deputados');
+  const deputados = response.data.dados;
+
+  return {
+    props: { deputados },
+  };
+}
---
+++ services/apiDeputados.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const apiDeputados = axios.create({
+    baseURL: 'https://dadosabertos.camara.leg.br/api/v2'
+})
+
+
+export default apiDeputados
